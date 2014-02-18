b29761757f4d34e40525ba09230f06a65d85aeb0
gjwang <guijiewan@gmail.com>
2014-02-17 18:03:19 -0800

    use exclude_exts as public configure

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
+#public configure
+exclude_exts=['.swx', '.swp', '.txt~', '.tmp', '.svn']
+
+
 #source/client configue
 wwwroot = '/data/www'
 monitorpath = wwwroot
 httphostname = 'http://192.168.5.60'
-
-exclude_exts=['.swx', '.swp', '.txt~', '.tmp', '.svn']
-
 broker = "redis://192.168.5.60:6379//"
 backend = "redis://192.168.5.60:6379//"
-
 
 
 #dest/worker configure
