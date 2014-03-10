3b023f31a3d5bd7f18b69abd238e8e3764c3e0d5
gjwang <guijiewan@gmail.com>
2014-03-10 02:32:52 -0700

    modifiy worker do whole sync threads num

--- config_worker.py
+++ config_worker.py
@@ -11,9 +11,9 @@ dstmonitorpath = dstwwwroot
 special_exts=['.m3u8']
 
 #thread num while doing whole sync
-HANDLER_THREAD_COUNT = 8
-TASK_QUEUE_MAX_SIZE = 30240
+HANDLER_THREAD_COUNT = 4
+TASK_QUEUE_MAX_SIZE = 65536
 
 #use 2**retry_times backoff methon, don't retry too much times unless you kwow what you are doing
-MAX_RETRY_TIMES = 7
+MAX_RETRY_TIMES = 6
 
